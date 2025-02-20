1fee337e31ba71345c41b7d3cf1a3e08b6a27c48
Moritz <moritz.seibold@stud.ph-gmuend.de>
2025-02-20 01:37:40 +0100

    "Refactored" View classes. Separated the large tabview into multiple user controls ("Tabs").

---
+++ LLMR/Views/Tabs/LinkGenerationTab.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LLMR.Views.Tabs;
+
+public partial class LinkGenerationTab : UserControl
+{
+    public LinkGenerationTab()
+    {
+        InitializeComponent();
+    }
+}
